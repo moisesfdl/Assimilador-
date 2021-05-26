df071f9720ca66c8e47bd7d314e3ff2877bccb6e
MetaDados&Correçoes <43420973+moisesfdl@users.noreply.github.com>
2021-05-26 01:38:47 -0300

    Update 2º OBa .cpp

--- 2º OBa .cpp
+++ 2º OBa .cpp
@@ -1,10 +1,10 @@
 //'©'moisesfdl_pbt@hotmail.com.br
 #include <activation.h>
 #include <>
-#include "OBa"
+#include "OB+a"
 
 #define OBa;
-OBa{ CPU == OBa[i5] '+=1' };
+OB+a{ CPU == OB+a[i5] '+=1' };
 
 
 long main()
@@ -13,23 +13,23 @@ long main()
     *(*CPU)ptr;
     Vetor[i5]{ moisesfdl };
     Vetor{ i5 };
-    OBa{ auto("audio" + ouvir)auto("ouvir" + dialeto)auto("dialeto" + log),sizeof("log" + dialeto),sizeof("log" + analise),sizeof("log" + responder)};
+    OB+a{ auto("audio" + ouvir)auto("ouvir" + dialeto)auto("dialeto" + log),sizeof("log" + dialeto),sizeof("log" + analise),sizeof("log" + responder)};
     std::cout << "CPU [" << CPU('OBa') << "] : " << std::endl;
     if (void)
     {
-        *(*OBa)ptr;
-        auto{OBa("audio" + log())};
-        OBa{log(audio)OBa{analise{"log()"+responde}}};
-        auto{OBa + "responder" + moisesfdl};
+        *(*OB+a)ptr;
+        auto{OB+a("audio" + log())};
+        OB+a{log(audio)OB+a{analise{"log()"+responde}}};
+        auto{OB+a + "responder" + moisesfdl};
         ptr{auto(void())};
     };  
-    for (OB i = 0; i > 0; i++)
+    for (OB+a i = 0; i > 0; i++)
     {
-    OBa{signbit(LONG64)?sizeof(void):OBaJ_ENHMETAFILE(LOG)};
-    OBa '+=1';
+    OB+a{signbit(LONG64)?sizeof(void):OB+aJ_ENHMETAFILE(LOG)};
+    OB+a '+=1';
     };
         std::cout << "CPU " << auto{void()} << " :" << std::endl;
-    OBa{ auto("audio" + ouvir)auto("ouvir" + dialeto)auto("dialeto" + log),sizeof("log" + dialeto),sizeof("log" + analise),sizeof("log" + responder)};
+    OB+a{ auto("audio" + ouvir)auto("ouvir" + dialeto)auto("dialeto" + log),sizeof("log" + dialeto),sizeof("log" + analise),sizeof("log" + responder)};
     system("CPU");
-    return OBa;
+    return OB+a;
 };
